f8de93e379715d251185bb9dbe521704678a3a1e
anas110440 <75642207+anas110440@users.noreply.github.com>
2022-03-22 12:53:52 +0300

    Kazoz

---
+++ js/plugins.js
@@ -0,0 +1,47 @@
+// JavaScript Document
+/*global $, document, window*/
+
+$(document).ready(function (){
+
+	
+
+	$('.carousel').carousel({
+		interval:3000
+		
+		
+	});
+
+	var scrollButton = $("#scroll-top");
+	var navbar1 = $(".navbar");
+	$(window).scroll(function ()
+	{
+		
+		if($(this).scrollTop()>=700)
+			{
+				scrollButton.show();
+			
+			}else{
+				scrollButton.hide();
+			}
+			});
+			// $(window).scroll(function ()
+			// {
+					
+			// if($(this).scrollTop()>=50)
+			// {
+			// 	navbar1.addClass('fixed-top');
+			// }else{
+			// 	navbar1.removeClass('fixed-top');
+			// }
+			// });
+	scrollButton.click(function (){
+			$("html,body").animate({ scrollTop : 0 }, 600 );
+		});
+
+
+
+
+
+	
+});
+
